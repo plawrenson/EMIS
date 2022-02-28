bc5db1691e8ce726d9a3a2a85fb32fe1854bd4ce
Philip Lawrenson <phl_lawrenson@hotmail.co.uk>
2022-02-28 23:23:51 +0000

    added output to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fhir.resources.bundle import Bundle
 from fhir.resources.patient import Patient
-
+from pandas import json_normalize
+import pandas as pd
 import pathlib
 import json
 filename = pathlib.Path(r"data\Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json")
@@ -12,10 +13,26 @@ Bun = Bundle.parse_file(filename)
 resources = []
 for entry in Bun.entry:
         if entry.request.url == 'Patient' and entry.request.method == 'POST':  
-                resources.append(entry.resource)
+                resources.append(entry.resource.json())
+
+
+
+temp = [] 
+
+for r in resources: 
+        temp.append(json.loads(''+r))
+
+df = json_normalize(temp)     
+
+df.to_parquet()
+
+#print(d)
+#df = pd.DataFrame(d)
+
+#print(df)     
 
 #loop through resources, set attributes and assign to object
-id = resources[0].id
+#id = resources[0].id
 
 
 
