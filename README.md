389e6ea52a3891d6e0ac1055ae8d00ced647dbc6
Philip Lawrenson <plawrenson@dfcapital.co.uk>
2022-02-28 14:59:30 +0000

    partial solution

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# EMIS
+very early attempt to use the fhir.resources library to pull information from Bundle files
+
+1.break bundle into resource entries - by method and resource type (url)
+
+2.loop through resources, extract attributes and assign to object
+
+3.output object to database/parquet etc
+
+
+#################################################
+
+I've also investigated pre-built FHIR servers using htttp and REST 
+
+I assumed this would be too simplistic to demonstrate a solution to this problem
+
+###############################################
+
+I've not had much time to devote to this so I can only submit a partial solution. 
+
+I can talk through it more fully along with altalternative methods to extract and store the data 
--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from fhir.resources.bundle import Bundle
 from fhir.resources.patient import Patient
+
 import pathlib
 import json
-filename = pathlib.Path(r"EMIS\data\Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json")
+filename = pathlib.Path(r"data\Aaron697_Dickens475_8c95253e-8ee8-9ae8-6d40-021d702dc78e.json")
 Bun = Bundle.parse_file(filename)
 
+
+#extract resources from Bundle
+#url and method could be made dynamic 
 resources = []
 for entry in Bun.entry:
+        if entry.request.url == 'Patient' and entry.request.method == 'POST':  
+                resources.append(entry.resource)
+
+#loop through resources, set attributes and assign to object
+id = resources[0].id
+
+
+
+""" resources = []
+for entry in Bun.entry:
     if entry.request.url == 'Patient':
-        resources.append(entry.resource)
+        resources.append(entry.resource.json())
+
+print(resources[0]) """
+
+""" pat = Patient.parse_obj(resources[0]).json()   
+
+urls = []
+
+for entry in Bun.entry:
+        urls.append(entry.request.url)
 
 
-pat = Patient.parse_obj(resources[0]).json()   
+urls = list(set(urls))
 
-print(pat)
+print (urls) """
